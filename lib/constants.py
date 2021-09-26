0cb3c8d4fd29216f852ef41cec4b3621a092ee02
FBi <florian.burri@csem.ch>
2021-09-26 02:22:54 +0200

    update constants

--- lib/constants.py
+++ lib/constants.py
@@ -4,5 +4,6 @@ DATA_DIR = Path(__file__).parent.parent / "data"
 ANOMALIES_FILE = DATA_DIR / "anomalies.json"
 
 EVENTS_FILE = DATA_DIR / "events.csv"
-DISRUPTIONS_FILE = DATA_DIR / "disruptions.csv"
-RSSI_FILE = DATA_DIR / "rssi.csv"
+DISRUPTIONS_FILE = DATA_DIR / "disruptions_L01.csv"
+RSSI_FILE = DATA_DIR / "rssi_L01.csv"
+VELOCITY_FILE = DATA_DIR / "velocities_L01.csv"

32be82e0e0e9c79de1d21dbd4330552099b1f1d8
Elia Bieri <contact@eliabieri.com>
2021-09-28 18:45:30 +0200

    Change initially loaded data set

--- lib/constants.py
+++ lib/constants.py
@@ -3,6 +3,6 @@ from pathlib import Path
 DATA_DIR = Path(__file__).parent.parent / "data"
 ANOMALIES_FILE = DATA_DIR / "anomalies.json"
 
-DISRUPTIONS_FILE = DATA_DIR / "disruptions_L01.csv"
-RSSI_FILE = DATA_DIR / "rssi_L01.csv"
-VELOCITY_FILE = DATA_DIR / "velocities_L01.csv"
+DISRUPTIONS_FILE = DATA_DIR / "disruptions_L01_D055.csv"
+RSSI_FILE = DATA_DIR / "rssi_L01_D055.csv"
+VELOCITY_FILE = DATA_DIR / "velocities_L01_D055.csv"
